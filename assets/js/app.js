3995e8b8f84548080612d80ff3cf5ef94b5d2ab5
Yana Kozarenko <event2016.2017@gmail.com>
2023-05-16 20:50:04 +0100

    fixed input

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,18 +5,10 @@ let cityHumidity = document.querySelector("#cityHumidity");
 let checkWeatherInCity = document.querySelector("#checkWeatherInCity");
 let apiKey = "cc8bb3bb7011ef268a462c26e462d900";
 
-let getCity = () => {
-  fetch(
-    "http://api.openweathermap.org/geo/1.0/direct?q=${cityInput.value},${state code},${country code}&appid=cc8bb3bb7011ef268a462c26e462d900"
-  )
-    .then((res) => res.json())
-    .then((data) => console.log(data))
-    .catch((error) => console.log(error));
-};
-
 let getWeather = () => {
+  let city = cityInput.value;
   fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=cc8bb3bb7011ef268a462c26e462d900"
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
   )
     .then((res) => res.json())
     .then((data) => console.log(data))
@@ -25,7 +17,7 @@ let getWeather = () => {
 
 let submitButton = (event) => {
   event.preventDefault();
-  getCity();
+  getWeather();
 };
 
 checkWeatherInCity.addEventListener("click", submitButton);
